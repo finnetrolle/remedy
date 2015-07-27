2fa7a1fc4a5faa6fcc0a823ddf45e8aa6306cacd
finnetrolle <maxsyachin@gmail.com>
2015-07-27 23:14:19 +0300

    initial commit

---
+++ src/main/java/ru/trollsmedjan/remedy/Remedy.java
@@ -0,0 +1,7 @@
+package ru.trollsmedjan.remedy;
+
+/**
+ * Created by finnetrolle on 27.07.2015.
+ */
+public class Remedy {
+}
---
+++ src/main/java/ru/trollsmedjan/remedy/model/Beacon.java
@@ -0,0 +1,7 @@
+package ru.trollsmedjan.remedy.model;
+
+/**
+ * Created by finnetrolle on 27.07.2015.
+ */
+public class Beacon {
+}
