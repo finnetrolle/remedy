31dfc476d75e97c14a0aaa3f609450e876db705b
finnetrolle <maxsyachin@gmail.com>
2015-08-08 03:03:54 +0300

    1.3.0 switched back to swagger

--- src/main/java/ru/trollsmedjan/remedy/configuration/SwaggerConfig.java
+++ src/main/java/ru/trollsmedjan/remedy/configuration/SwaggerConfig.java
@@ -1,31 +1,31 @@
 package ru.trollsmedjan.remedy.configuration;
 
-//import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-//import com.mangofactory.swagger.plugin.*;
-//import com.wordnik.swagger.model.ApiInfo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.context.annotation.*;
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.*;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.*;
 
 
-//@Configuration
-//@EnableSwagger
-//@EnableAutoConfiguration
+@Configuration
+@EnableSwagger
+@EnableAutoConfiguration
 public class SwaggerConfig  {
 
-//    private SpringSwaggerConfig springSwaggerConfig;
-//
-//    @Autowired
-//    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-//        this.springSwaggerConfig = springSwaggerConfig;
-//    }
-//
-//    @Bean
-//    public SwaggerSpringMvcPlugin customImplementation() {
-//        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-//                .apiInfo(new ApiInfo("remedy", "Fozzie-sov campaigns automata", null, "maxsyachin@gmail.com", null, null))
-//                .includePatterns("/campaign.*", "/constellation.*", "/monitoring.*", "/complex.*");
-//    }
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiInfo(new ApiInfo("remedy", "Fozzie-sov campaigns automata", null, "maxsyachin@gmail.com", null, null))
+                .includePatterns("/campaign.*", "/constellation.*", "/monitoring.*", "/complex.*");
+    }
 
 
 }
